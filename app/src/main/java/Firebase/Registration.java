7206bd8f02f24d9127f5f1969d960342e479cc4f
MovieApp <pialkhanpial@gmail.com>
2021-05-17 00:34:44 +0600

    Movieapp

---
+++ app/src/main/java/Firebase/Registration.java
@@ -0,0 +1,53 @@
+package Firebase;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.movieapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class Registration extends Fragment {
+
+    private EditText email, password;
+    private FirebaseAuth auth;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        email = view.findViewById(R.id.editTextTextPersonName) ;
+        password = view.findViewById(R.id.editTextTextPassword) ;
+
+
+        auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                        } else {
+
+                        }
+                    }
+                });
+    }
+}
